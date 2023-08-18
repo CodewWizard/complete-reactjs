38895aae23cd42def2da19c47612f87a95fd9c90
Madiha Mujawar <mmujawar@cogitate.us>
2023-08-18 12:12:05 +0530

    adding expenses-filter

--- 01-setup__hooks/src/components/Expenses/Expenses.jsx
+++ 01-setup__hooks/src/components/Expenses/Expenses.jsx
@@ -1,12 +1,37 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from '../UI/Card'
+import React, {useState} from 'react';
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props){
+  const [filteredYear, setFilteredYear] = useState('2020');
+  //const [filterInfoText, setFilterInfoText] = useState('2019, 2021 & 2022');
+
+  let filterInfoText = '2019, 2021 & 2022';
+  
+    if(filteredYear === '2019'){
+      filterInfoText ='2020, 2021 & 2022';
+    }
+    else if(filteredYear === '2020'){
+      filterInfoText ='2019, 2021 & 2022';
+    }
+    else if(filteredYear === '2021'){
+      filterInfoText = '2019, 2020 & 2022';
+    }
+    else{
+      filterInfoText = '2019, 2020 & 2021';
+    }
+
+  function filterChangeHandler(selectedYear){
+    setFilteredYear(selectedYear);
+  }
 //comment
   return(
     <>
       <Card className = "expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        <p style={{color:"white"}}>Data for years {filterInfoText} is hidden</p>
         <ExpenseItem 
         title={props.items[0].title}
         date={props.items[0].date} 
