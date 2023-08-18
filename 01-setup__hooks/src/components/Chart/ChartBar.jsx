4373c59c62ea1c31f46775f999baac896dac6f8b
Madiha Mujawar <madihamujawar410@gmail.com>
2023-08-18 19:33:18 +0530

    modify in chart component

--- 01-setup__hooks/src/components/Chart/ChartBar.jsx
+++ 01-setup__hooks/src/components/Chart/ChartBar.jsx
@@ -1,3 +1,14 @@
-function ChartBar(){
-    
+import React from 'react';
+import './ChartBar.css'
+
+const ChartBar =(props)=>{
+    return(
+        <>
+            <div className='chart-bar'>
+
+            </div>
+        </>
+    )
 }
+
+export default ChartBar;
