e1ca18087509df849d14c722693c89a814c73c23
Madiha Mujawar <madihamujawar410@gmail.com>
2023-08-24 18:53:13 +0530

    modify add

--- 06-practice-project/src/App.js
+++ 06-practice-project/src/App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 import AddUser from './components/Users/AddUser'
+import UsersList from './components/Users/UsersList'
 
 export default function App() {
+  const [usersList, setUsersList] = useState([]); 
+
+  const addUserHandler =(uName, uAge)=>{
+    setUsersList((prevUsersList)=>{
+      return [...prevUsersList, 
+      {
+        name: uName,
+        age : uAge,
+        id : Math.random().toString()
+      }]
+    })
+  }
   return (
     <>
-      <AddUser/>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </>
   )
 }
