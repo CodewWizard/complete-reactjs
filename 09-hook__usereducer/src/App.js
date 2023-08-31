f11ece68763d84ab9eb602695d7d4d79ef1e29fd
Madiha Mujawar <madihamujawar410@gmail.com>
2023-08-31 19:25:17 +0530

    modify App.js

--- 09-hook__usereducer/src/App.js
+++ 09-hook__usereducer/src/App.js
@@ -1,10 +1,13 @@
 import Counter from './Intro/Counter';
+import Todo from './Todo/Todo'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Counter/>
+      <br/>
+      <Todo/>
     </div>
   );
 }
