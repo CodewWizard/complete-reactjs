f12355d405540acaa909e4a780b7e15432adf847
Madiha Mujawar <madihamujawar410@gmail.com>
2023-08-31 17:00:25 +0530

    move it into intro directory

--- 09-hook__usereducer/src/App.js
+++ 09-hook__usereducer/src/App.js
@@ -1,46 +1,12 @@
-import './App.css'
-import React, {useState, useReducer} from 'react';
+import Counter from './Intro/Counter';
+import './App.css';
 
-const ACTIONS = {
-  INCREMENT : 'increment',
-  DECREMENT : 'decrement'
-}
-
-const reducer =(state, action)=>{
-    switch(action.type){
-      case 'increment' : 
-        return {count : state.count + 1}
-      case 'decrement' : 
-        return {count : state.count - 1}
-      default : 
-        return state 
-    }
-}
-
-export default function App() {
-
-  // current state, used to dispatch new action,
-  // reducer returns new updated state 
-  const [state, dispatch] = useReducer(reducer, {count : 0})
-
-  const increment = () => {
-    dispatch({
-      type : ACTIONS.INCREMENT
-    })
-  }
-
-  const decrement = () => {
-    dispatch({
-      type : ACTIONS.DECREMENT
-    })
-  }
+function App() {
   return (
-    <div className='App'>
-      <h1>useReducer</h1>
-      <br/>
-      <button className='btn btn-primary' onClick={decrement}>-</button> &nbsp; &nbsp;
-      <span style={{fontWeight:'bold'}}>{state.count}</span>  &nbsp; &nbsp;
-      <button className='btn btn-primary' onClick={increment}>+</button>
+    <div className="App">
+      <Counter/>
     </div>
-  )
+  );
 }
+
+export default App;
---
+++ 09-hook__usereducer/src/Intro/Counter.js
@@ -0,0 +1,45 @@
+import React, {useState, useReducer} from 'react';
+
+const ACTIONS = {
+  INCREMENT : 'increment',
+  DECREMENT : 'decrement'
+}
+
+const reducer =(state, action)=>{
+    switch(action.type){
+      case 'increment' : 
+        return {count : state.count + 1}
+      case 'decrement' : 
+        return {count : state.count - 1}
+      default : 
+        return state 
+    }
+}
+
+export default function Counter() {
+
+  // current state, used to dispatch new action,
+  // reducer returns new updated state 
+  const [state, dispatch] = useReducer(reducer, {count : 0})
+
+  const increment = () => {
+    dispatch({
+      type : ACTIONS.INCREMENT
+    })
+  }
+
+  const decrement = () => {
+    dispatch({
+      type : ACTIONS.DECREMENT
+    })
+  }
+  return (
+    <div>
+      <h1>useReducer</h1>
+      <br/>
+      <button className='btn btn-primary' onClick={decrement}>-</button> &nbsp; &nbsp;
+      <span style={{fontWeight:'bold'}}>{state.count}</span>  &nbsp; &nbsp;
+      <button className='btn btn-primary' onClick={increment}>+</button>
+    </div>
+  )
+}
