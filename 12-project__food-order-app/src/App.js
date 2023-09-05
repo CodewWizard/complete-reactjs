c25530d1d3165e86258983e2fe8fb467d5271196
Madiha Mujawar <madihamujawar410@gmail.com>
2023-09-05 15:44:21 +0530

    setting up and adding comp, assets, layouts..

---
+++ 12-project__food-order-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <main>
+        <Meals/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ 12-project__food-order-app/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import mealsImg from '../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header =(props)=>{
+    return(
+        <>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton/>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImg} alt='a table of delicious food' />  
+            </div>
+        </>
+    )
+}
+
+export default Header;
