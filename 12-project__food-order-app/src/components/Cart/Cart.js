97f948edc0f3e2aab9291154aa4ded6a82139b45
Madiha Mujawar <madihamujawar410@gmail.com>
2023-09-08 17:01:49 +0530

    outputting cart items

--- 12-project__food-order-app/src/components/Cart/Cart.js
+++ 12-project__food-order-app/src/components/Cart/Cart.js
@@ -1,11 +1,32 @@
-import classes from './Cart.module.css';
-import Modal from '../UI/Modal';
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCxt = useContext(CartContext);
+
+  const totalAmount = `$${cartCxt.totalAmount.toFixed(2)}`;
+  const hasItems = cartCxt.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+    <ul className={classes["cart-items"]}>
+      {cartCxt.items.map((item) => (
+        <li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        </li>
       ))}
     </ul>
   );
@@ -15,11 +36,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
