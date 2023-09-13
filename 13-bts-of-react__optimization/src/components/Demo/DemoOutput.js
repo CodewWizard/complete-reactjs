7c7c9f51478d36347251a9cc5fe3547175dca3c4
Madiha Mujawar <madihamujawar410@gmail.com>
2023-09-13 19:28:23 +0530

    modification

--- 13-bts-of-react__optimization/src/components/Demo/DemoOutput.js
+++ 13-bts-of-react__optimization/src/components/Demo/DemoOutput.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import MyParagraph from './MyParagraph';
 
-export default function DemoOutput(props) {
+const DemoOutput =(props)=> {
   console.log("DemoOutput RUNNING");
   return (
     <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>
   )
 }
+
+
+// react.memo() is used to optimize functional components
+export default React.memo(DemoOutput);
