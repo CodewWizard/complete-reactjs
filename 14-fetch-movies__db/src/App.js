11e4a154d4cde2cf35a8d9102678b10d04738efc
Madiha Mujawar <madihamujawar410@gmail.com>
2023-09-22 12:27:51 +0530

    Handling Loading and data states

--- 14-fetch-movies__db/src/App.js
+++ 14-fetch-movies__db/src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler(){
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films');
     const data = await response.json();
       const transformedmovies = data.results.map((movieData)=>{
@@ -17,6 +19,7 @@ function App() {
         }
       })
       setMovies(transformedmovies);
+      setIsLoading(false)
   }
 
   return (
@@ -25,7 +28,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </>
   );
