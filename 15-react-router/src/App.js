2c0d31004c014b2dc22929e855e730fc3ed1abb9
Madiha Mujawar <madihamujawar410@gmail.com>
2023-09-29 11:49:28 +0530

    navigating between pages with links

--- 15-react-router/src/App.js
+++ 15-react-router/src/App.js
@@ -1,12 +1,26 @@
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements} from 'react-router-dom';
 import { Home } from './routes/Home';
 import './App.css';
+import { Products } from './routes/Products';
 
-const router = createBrowserRouter([
-  {path: '/'},
-  {path: '/home', element: <Home />},
-  {}
-])
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path='/home' element={<Home/>} />
+    <Route path='/products' element={<Products/>} />
+  </Route>
+);
+
+// const router = createBrowserRouter([
+//   {path: '/'},
+//   {path: '/home', element: <Home />},
+//   {}
+// ])
+
+const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return (
--- 15-react-router/src/routes/Home.js
+++ 15-react-router/src/routes/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export const Home = () => {
   return (
     <>
         <h1>Home page</h1>
+        <p>with href and achor tag <a href='/products'>list of products</a></p>
+        <p>with Link from react-router-dom <Link to='/products'>list of products</Link></p>
     </>
   )
 }
