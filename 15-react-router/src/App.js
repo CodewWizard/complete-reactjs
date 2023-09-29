f31e20108684ab1e34f370e068277286ad9310a8
Madiha Mujawar <madihamujawar410@gmail.com>
2023-09-29 11:06:43 +0530

    defining and adding routes

--- 15-react-router/src/App.js
+++ 15-react-router/src/App.js
@@ -1,9 +1,18 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { Home } from './routes/Home';
 import './App.css';
+
+const router = createBrowserRouter([
+  {path: '/'},
+  {path: '/home', element: <Home />},
+  {}
+])
 
 function App() {
   return (
     <div className="App">
       <h1>react router</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ 15-react-router/src/routes/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <>
+        <h1>Home page</h1>
+    </>
+  )
+}
